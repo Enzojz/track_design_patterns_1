43ee9f94bc32bbb45d2db81ddeb06e952c1906cc
Enzojz <enzojz@gmail.com>
2017-08-08 01:23:33 +0200

    First commit

---
+++ mod.lua
@@ -0,0 +1,21 @@
+﻿function data()
+    return {
+        info = {
+            minorVersion = 0,
+            severityAdd = "NONE",
+            severityRemove = "WARNING",
+            name = _("name"),
+            description = _("desc"),
+            authors = {
+                {
+                    name = "Enzojz",
+                    role = "CREATOR",
+                    text = "Idee, Scripting",
+                    steamProfile = "enzojz",
+                    tfnetId = 27218,
+                },
+            },
+            tags = {"Train Station", "Station", "Bridge", "Track"},
+        },
+    }
+end
---
+++ strings.lua
@@ -0,0 +1,22 @@
+local descEN = [[]]
+
+local descFR = [[]]
+
+local descCN = [[]]
+
+function data()
+    return {
+        en = {
+            ["name"] = "Parameterized tracks",
+            ["desc"] = descEN,
+        },
+        fr = {
+            ["name"] = "Voies paramétrés",
+            ["desc"] = descFR,
+        },
+        zh_CN = {
+            ["name"] = "参数化轨道",
+            ["desc"] = descCN,
+        },
+    }
+end
