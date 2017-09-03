b3fc255fb559481985520d862b2feaa49c0b5561
Enzojz <enzojz@gmail.com>
2017-09-04 01:01:47 +0200

    Tracks with signals

---
+++ res/config/tunnel/void_entry.lua
@@ -0,0 +1,12 @@
+function data()
+return {
+	name = _("Tunnel void"),
+	carriers = { "RAIL" },
+	portals = {
+		{ "track_design_patterns/void.mdl", "track_design_patterns/void.mdl", "track_design_patterns/void.mdl" },
+		{ "track_design_patterns/void.mdl", "track_design_patterns/void.mdl", "track_design_patterns/void.mdl" },
+		{ "track_design_patterns/void.mdl", "track_design_patterns/void.mdl", "track_design_patterns/void.mdl" },
+	},
+	cost = 0.0
+}
+end
